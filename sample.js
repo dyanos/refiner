62585cf116d2b38672bcefc1c98d39a9d69f9f2a
Sangjin Sim <dyanos@Macintosh.local>
2013-12-07 01:40:30 +0900

    removed a redundant file

--- sample.js
+++
@@ -1,2 +0,0 @@
-var csv = require('csv');
-csv().from.path('tmp/130304100734.real.csv', {columns: true}).on('record', function(row, index) { console.log('#'+index+' '+row["회원이름"]); }).on('end', function(count) {});
